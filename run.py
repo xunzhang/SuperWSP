dc6c2d033de6857c3244a601ebdd09476b180c49
wuhong <xunzhangthu@gmail.com>
2012-10-21 16:34:34 +0800

    modify the entry mode

--- run.py
+++ run.py
@@ -4,11 +4,17 @@
 
 __author__ = ['Hong Wu<xunzhangthu@gmail.com>']
 
+import sys
 from load import LoadInput
 from superWSP import SuperWordSearchPuzzle
 
 if __name__ == '__main__':
-  input_obj = LoadInput('input.txt')
+  
+  if len(sys.argv) != 2:
+    print 'Illegal input!!'
+    sys.exit()
+  
+  input_obj = LoadInput(sys.argv[1])
   grid, is_wrap, swords = input_obj.getData()
   search_obj = SuperWordSearchPuzzle(grid, swords, is_wrap)
   search_obj.find_paths()
